a82c9a92be862705311d8d3a72599d82cd86f446
YDEK693 <82566397+YDEK693@users.noreply.github.com>
2023-10-25 09:38:48 +0200

    caisse

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
 
+# This represents the player's inertia.
+var push_force = 80.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -24,5 +26,9 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
+	for i in get_slide_collision_count():
+		var c = get_slide_collision(i)
+		if c.get_collider() is RigidBody2D:
+			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
--- ccaisse.gd
+++ ccaisse.gd
@@ -1,11 +1,14 @@
 extends RigidBody2D
 
-
+var current_stage = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
+func go_next_stage():
+	current_stage += 1
+	get_tree().change_scene("res://Scenes/Levels/Level "+str(current_stage)+".xml")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
