ad6faff38f5370178616ad07312b0db00876b054
Julien <101173576+JulienJaffre1@users.noreply.github.com>
2023-10-26 08:32:53 +0200

    Mini modif

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400
-const push_force = 40.0
+const push_force = 20.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
