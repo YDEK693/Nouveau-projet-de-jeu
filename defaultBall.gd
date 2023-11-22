03ce0df38eef967582644c440c0b0720ea15e035
YDEK693 <82566397+YDEK693@users.noreply.github.com>
2023-11-22 09:42:54 +0100

    moving plateforme de ses morts ok

--- defaultBall.gd
+++ defaultBall.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 const push_force = 80.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var sens = 1 
 # Création d'une énumération pour les états
 enum State {
 	DEFAULT,
@@ -19,37 +19,16 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	
 	var SPEED = SPEEDDIC[current_state]
 	var JUMP_VELOCITY = JUMPDIC[current_state]
 	#gauche droite jump
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
 	
-	if current_state == State.STICKY:
-		if(is_on_wall() || is_on_ceiling()):
-			velocity.y = 0
-			#var velo = -get_real_velocity()
-			#velo.y = 0
-			#move_and_collide(velo*delta)	
-		else:
-			velocity.y += gravity * delta
-			
-	else:	
-		velocity.y += gravity * delta	
-	if(is_on_floor()):
-		print('flor')
-		velocity.y = JUMP_VELOCITY*2
-	
+	#balle de tennis
+#	if(velocity.y>0 && is_on_floor()):
+#		velocity.y += JUMP_VELOCITY*2
+
 	move_and_slide()
-		
+
 	if Input.is_action_just_pressed("ui_focus_next"):
 		if current_state == State.DEFAULT:
 			current_state = State.STICKY
@@ -59,11 +38,43 @@ func _physics_process(delta):
 	#push caisse
 	for i in get_slide_collision_count():
 		var c = get_slide_collision(i)
-		if c.get_collider() is RigidBody2D:
-			c.get_collider().apply_central_impulse(-c.get_normal() * push_force)
+		if c.get_collider() is AnimatableBody2D:
+			if c.get_collider().position.x > 490:
+				sens = -1 
+			if c.get_collider().position.x < 10:
+				sens = 1
 			
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 
+	if current_state == State.STICKY:
+		print(is_on_ceiling(),is_on_wall())
+#		if(is_on_ceiling()):
+#			velocity.y = 0
+#			velocity.x = 400*sens;
+#		if(is_on_wall()):
+#			velocity.y = 0
+#			velocity.x = 500*sens;
+		if(is_on_ceiling()||is_on_wall()):
+			velocity.x = 200*sens;
+		else:
+			velocity.y += gravity * delta
+
+	else:	
+		velocity.y += gravity * delta	
+
+	
+# if needed for bouncing of walls or ceilings
+#var was_on_wall := false
+#var was_on_ceiling
+	
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Platform"):
 		print("lol")
