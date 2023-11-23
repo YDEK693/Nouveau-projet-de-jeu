e647377f04262897c3cd8214bcd00b189cf7c0d3
YDEK693 <82566397+YDEK693@users.noreply.github.com>
2023-11-23 18:01:57 +0100

    correction switch lvl + kill pic

--- Script/kill.gd
+++ Script/kill.gd
@@ -14,6 +14,4 @@ func _process(delta):
 func _on_body_entered(body):
 	if body.is_in_group("Object"):
 		var current_scene_file = get_tree().current_scene.scene_file_path
-		var next_level_number = current_scene_file.to_int() + 1
-		var next_level_path = "res://Niveaux/"+str(next_level_number)+".tscn"
-		get_tree().change_scene_to_file(next_level_path)
+		get_tree().change_scene_to_file(current_scene_file)
